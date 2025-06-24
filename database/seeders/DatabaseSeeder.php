6e5f8f9dc16cf60e6541fd65b767b4fc7671ff87
silenthill99 <florian.graziani@sfr.fr>
2025-06-24 13:56:01 +0200

    Correction d'une faute d'orthographe

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(USerSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(ArticleSeeder::class);
     }
 }
