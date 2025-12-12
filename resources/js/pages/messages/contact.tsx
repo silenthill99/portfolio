16b4f9189de07eceaf27c95ba59b3c0055742f4e
silenthill99 <florian.graziani@sfr.fr>
2025-12-12 23:46:12 +0100

    Mise à jour du projet

--- resources/js/pages/messages/contact.tsx
+++ resources/js/pages/messages/contact.tsx
@@ -4,7 +4,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
-import { store } from '@/routes/contact';
+import { store } from '@/actions/App/Http/Controllers/MessageController';
 
 type FormProps = {
     pseudo: string;
